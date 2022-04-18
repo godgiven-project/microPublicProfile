562f71044f4623a5a939cf17981a767bd9cbb184
asmanp2012 <asmanp2012@gmail.com>
2022-04-19 03:21:11 +0430

    feat: .env

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,18 @@ import {
   pageHome,
   pagePublicProfile,
 } from './page/index.js';
+import { config } from 'dotenv';
+
+config();
+console.log();
+
+// envierment
+const port = parseInt(process.env.port as string) as number | 'NaN';
+const vertion = process.env.ver ?? 'v1';
 
 const app = new App();
-app.port = 5000;
-app.version = 'v1';
+app.port = port !== 'NaN' ? port : 5000;
+app.version = vertion;
 app.middlewareList.push(authFunction);
 
 app.register('GET', '/', pageHome);
